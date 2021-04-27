dfb96b5261de0fd6d077a10844ab2a1e20229452
monsherko <33561511+monsherko@users.noreply.github.com>
2021-04-27 22:57:25 +0300

    Create domain2ip.sh

---
+++ opt/domain2ip.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+SEP=';'
+OUTFILE="pair_ip.l"
+
+while read line;                                                    1 ⨯ 3 ⚙
+do
+  # ping with timing 1 sec and one shot
+  str_msg=$(ping -t 1 -c 1 $line |  grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" | head -n 1);
+
+
+  if [[ $str_msg =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+   echo "${line};${str_msg}" >> $(OUTFILE)
+  fi
+  
+done < target.list
